38e2a0fe60b74161a3c085e3eb8c3ab6001d33b0
Nicolas Nagano <nicolasjap@hotmail.com>
2023-06-10 03:25:52 -0300

    add_lines

--- app/src/main/java/devandroid/nicolas/applistacurso/model/Pessoa.java
+++ app/src/main/java/devandroid/nicolas/applistacurso/model/Pessoa.java
@@ -2,4 +2,44 @@ package devandroid.nicolas.applistacurso.model;
 
 public class Pessoa {
 
+    private String nome;
+    private String sobrenome;
+    private String curso;
+    private String telefone;
+
+    public Pessoa(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
--- app/src/main/java/devandroid/nicolas/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/nicolas/applistacurso/view/MainActivity.java
@@ -3,6 +3,9 @@ package devandroid.nicolas.applistacurso.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import devandroid.nicolas.applistacurso.R;
 import devandroid.nicolas.applistacurso.model.Pessoa;
@@ -11,9 +14,26 @@ public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
 
+    EditText txtNome;
+    EditText txtSobrenome;
+    EditText txtNomeCurso;
+    EditText txtTelContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLimpar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void OnClick(view v){
+
+            }
+        });
+
     }
 }
